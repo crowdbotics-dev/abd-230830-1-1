c1187223c7aa0bce0fd1ed5d67081309ed047d06
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-31 09:04:50 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Home(models.Model):
     'Generated Model'
     number = models.BigIntegerField()
+    name = models.CharField(max_length=255,null=True,blank=True,)
